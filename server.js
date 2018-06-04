bc24b7e4614d3f48c8e6bed58571a5729cbf5dc4
danhn255 <danhn255@gmail.com>
2018-06-03 18:03:01 -0700

    added web page

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const http = require('http');
-
-
+let light1Level = null;
 
 function getEndpoint(url)
 {
@@ -14,14 +13,35 @@ function getParameter(url,endpoint)
 	url = url.substr(endpoint.length + 2,url.length);
 	const params = url.split("&");
 	const data = params.map((val) => val.split("="));
-	console.log(data);
+}
+
+
+function updateData(dataToUpdate,valueToUpdate)
+{
+	if (dataToUpdate == "light1")
+		light1Level = valueToUpdate;
+}
+
+function updateAllData(allData)
+{
+	allData.map((val) => updateData(val[0],val[1]))
+}
+
+function sendBackData(dataToRetrieve,res)
+{
+	console.log(dataToRetrieve);
+	if (dataToRetrieve[0][1] == "light1")
+		res.end(light1Level);
 }
 
 function main(req,res)
 {
 	const endpoint = getEndpoint(req.url);
-	getParameter(req.url,endpoint);
-	console.log(endpoint);
+	const data = getParameter(req.url,endpoint);
+	if (endpoint == "data")
+		updateAllData(endpoint,data);
+	else if (endpoint == "retrieveData")
+		sendBackData(data,res);
 	res.end("");
 }
 
